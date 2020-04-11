815bc6f0b4e60f9e46f8ccc0a32b05c06f552a6a
Abhinandan Srinivas <40843626+asri6725@users.noreply.github.com>
2020-04-11 15:56:22 +1000

    Update run.py
    Added a new port to test the connection with uni server.

--- run.py
+++ run.py
@@ -22,13 +22,13 @@ import controller
 #-----------------------------------------------------------------------------
 # It might be a good idea to move the following settings to a config file and then load them
 # Change this to your IP address or 0.0.0.0 when actually hosting
-host = 'localhost'
+host =  10.86.163.196 #'localhost'
 
 # Test port, change to the appropriate port to host
 port = 8080
 
 # Turn this off for production
-debug = True
+debug = Flase
 #-----------------------------------------------------------------------------
 
 #Run the server
