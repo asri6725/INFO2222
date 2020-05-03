faa031d9c07cdd77c029a093de2e13113bc0359c
Jackey Song <kson2621@uni.sydney.edu.au>
2020-05-03 22:01:38 +1000

    new sql function added - comment for what function to use in model.py

--- studhelp_dbsql.py
+++ studhelp_dbsql.py
@@ -32,11 +32,72 @@ def check_signup(username, password):
 	else:
 		return 0
 
+def get_user_subject(username):
+
+	subjects = []
+	count = 0
+
+	cursor.execute("""
+		SELECT S.subject_id
+		FROM user_detail UD INNER JOIN user_subject US USING(username) INNER JOIN subject S USING(subject_id)
+		WHERE UD.username = :username
+		ORDER BY S.subject_id
+		""", {"username": username})
+	data = cursor.fetchall()
+	if (len(data) == 0):
+		subjects = ["Not Initalised"]
+	else:
+		for record in data:
+			subjects.append(record[0])
+			count += 1
+	
+	return subjects
+
+#Return -1 when user is already enrolled in the unit
+def unit_add(username, unit):
+	
+	subjects = get_user_subject(username);
+	
+	for subject in subjects:
+		if subject == unit:
+			return subjects
+
+	cursor.execute("INSERT INTO user_subject VALUES(?, ?)", (username, unit));
+	connection.commit()
+
+	return get_user_subject(username)
+
+def get_post_contents(subject, title):
+
+	cursor.execute("""
+		SELECT P.title, P.subject_id, P.context
+		FROM post P
+		WHERE P.title = :title AND P.subject_id = :subject_id
+		""", {"title": title, "subject_id": subject})
+	data = cursor.fetchall()
+	return data[0]
+	# print(data[0])
+
+def get_all_post_title(unit):
+	cursor.execute("""
+		SELECT P.title
+		FROM post P
+		WHERE subject_id = :subject_id
+		ORDER BY P.post_id""", {"subject_id": unit})
+	data = cursor.fetchall()
+
+	title = []
+	for record in data:
+		title.append(record[0])
+	return title
+
+def add_new_post(username, subject, title, content):
+	cursor.execute("INSERT INTO post VALUES(?, ?, ?, ?)", (title, content, username, subject))
+	connection.commit()
+	return 0
+
 def add_user(username, password):
 	cursor.execute("INSERT INTO user_detail VALUES(?, ?, ?)", (username, password, 1))
 	connection.commit()
+	return 0
 
-#cursor.execute("INSERT INTO user_detail VALUES('jackey', 'password', 1)")
-#connection.commit()
-
-#cursor.execute("INSERT INTO user_detail VALUES('aa', 'bb')")
